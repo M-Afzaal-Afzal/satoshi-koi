84a0c130ce312d64c696777aec46937485791a7e
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-09 22:12:02 +0500

    responsiveness bug fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,8 +8,7 @@ import { FaDiscord } from "react-icons/fa";
 import { Link } from "react-scroll";
 import Typography from "@mui/material/Typography";
 import { FaInstalod } from "react-icons/fa";
-import Image from 'next/image';
-
+import Image from "next/image";
 
 function Navbar() {
   const [hide, showNav] = useState(true);
@@ -24,40 +23,59 @@ function Navbar() {
       maxWidth="xl"
       id="container-nav"
       sx={{
-          position:'relative',
+        position: "relative",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#1d1e60",
         height: "120px",
-        px:0,
+        px: 0,
       }}
     >
       <Box
         component="div"
         className="max-width"
         sx={{
-            position: 'fixed',
-            zIndex: 9999,
-            top: 0,
-            left: 0,
-            height: 'auto',
+          position: "fixed",
+          zIndex: 9999,
+          top: 0,
+          left: 0,
+          height: "auto",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
           backgroundColor: "#1d1e60",
-          // height: "100%",
           width: "100%",
-            px: '2rem',
+          px: {
+            md: 3,
+            sm: 1,
+          },
           borderBottom: "3px solid #FBFAF5",
-          // px:0
         }}
       >
-        <Box component="div" id="left-nav" sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+        <Box
+          component="div"
+          id="left-nav"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Image width={67} height={122} id="logo" src="/logo.png" alt="Logo" />
-          <Typography variant="h4" component="div" gutterBottom sx={{fontWeight:'800',color:'#FBFAF5',fontFamily:'Montserrat',fontSize:{
-              xs:"20xp"
-          }}} >
+          <Typography
+            variant="h4"
+            component="div"
+            gutterBottom
+            sx={{
+              fontWeight: "800",
+              color: "#FBFAF5",
+              fontFamily: "Montserrat",
+              fontSize: {
+                xs: "20xp",
+              },
+            }}
+          >
             Satoshi koi
           </Typography>
         </Box>
@@ -98,7 +116,7 @@ function Navbar() {
               duration={500}
               className={"nav-link"}
               onClick={sidebarHandler}
-              to={'SNEAK'}
+              to={"SNEAK"}
               spy={true}
               activeClass={"active"}
             >
@@ -127,25 +145,25 @@ function Navbar() {
               Mint
             </Link>
             <Box
-          component="div"
-          className="social-icon2"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            display:'none'
-          }}
-        >
-          <a href="#">
-            <TwitterIcon sx={{ fontSize: 45 }} />
-          </a>
-          <a href="#">
-            <FaDiscord size={45} />
-          </a>
-          <a href="#">
-            <FaInstalod size={45} />
-          </a>
-        </Box>
+              component="div"
+              className="social-icon2"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                display: "none",
+              }}
+            >
+              <a href="#">
+                <TwitterIcon sx={{ fontSize: 45 }} />
+              </a>
+              <a href="#">
+                <FaDiscord size={45} />
+              </a>
+              <a href="#">
+                <FaInstalod size={45} />
+              </a>
+            </Box>
           </Box>
         </Box>
         <Box
@@ -161,10 +179,10 @@ function Navbar() {
             <TwitterIcon sx={{ fontSize: 40 }} />
           </a>
           <a href="#">
-            <FaDiscord size={40}  />
+            <FaDiscord size={40} />
           </a>
           <a href="#">
-            <FaInstalod size={40}  />
+            <FaInstalod size={40} />
           </a>
         </Box>
         <Box component="div" className="res-nav">
